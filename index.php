d4c88a3a27af564de8cab357b956b11f0762ef2e
AMAN <amanbharti19@gmail.com>
2015-10-09 21:55:53 +0530

    delete file

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require("../templates/facebook.php");
-    require("../templates/header.php");
-    require("../templates/navbar.php");
-    require("../templates/login_modal.php");
-
-  
-    require("../templates/footer.php");
-?>
